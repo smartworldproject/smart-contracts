fb18de60b6258aaa85d3e0e7d57603b7fd8ca9b2
b3hr4d <behradmusiclove@yahoo.com>
2022-09-18 19:02:28 +0300

    update smartland contract

--- contracts/SmartLand/SmartLand.sol
+++ contracts/SmartLand/SmartLand.sol
@@ -17,11 +17,13 @@ contract SmartLand is ERC721Enumerable, SmartSecure {
   }
 
   mapping(address => UserStruct) public users;
-  mapping(uint256 => bytes) public landDetail;
 
   uint256 maxSupply = 10_000;
 
-  constructor() ERC721("SmartLand", "STL") {}
+  constructor() ERC721("SmartLand", "STL") {
+    owner = msg.sender;
+    gameMaster = msg.sender;
+  }
 
   function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
     return string(abi.encodePacked(BASE_URL, tokenId.toString(), BASE_EXT));
@@ -30,52 +32,65 @@ contract SmartLand is ERC721Enumerable, SmartSecure {
   function mint(
     address referrer,
     uint256 tokenId,
-    bytes memory details
+    bytes memory data
   ) public payable {
-    require(tokenId < maxSupply && !_exists(tokenId), "NOT");
-    require(totalSupply() <= maxSupply, "MAX");
-    require(msg.value >= LAND_PRICE, "ETH");
+    require(tokenId < maxSupply && !_exists(tokenId), "Error::SmartLand, NFT does not exist!");
+    require(msg.value >= LAND_PRICE, "Error::SmartLand, Incorrect Value!");
+    require(data.length == 16, "Error::SmartLand, Incorrect Data!");
+
     transferToOwner();
 
-    landDetail[tokenId] = details;
+    landData[tokenId] = data;
 
-    if (!exists(_msgSender())) {
-      if (exists(referrer)) {
+    if (notExist(_msgSender())) {
+      if (referrer == address(0)) {
+        users[_msgSender()].referrer = address(1);
+      } else if (exist(referrer)) {
         users[_msgSender()].referrer = referrer;
 
-        payReferrer(users[_msgSender()].referrer);
+        payReferrer(referrer);
       } else {
-        users[_msgSender()].referrer = address(1);
+        revert("Error::SmartLand, Referrer does not exist!");
       }
     }
 
     _safeMint(msg.sender, tokenId);
   }
 
-  function payReferrer(address referrer) internal {
+  function payReferrer(address referrer) private {
     uint256 refAmount = LAND_PRICE.percent(REFERRAL_PERCENT);
-    users[referrer].refAmounts += refAmount;
+    users[referrer].refAmounts = users[referrer].refAmounts.add(refAmount);
 
     emit ReferralReceived(referrer, _msgSender(), refAmount);
   }
 
   function withdrawInterest() public nonReentrant {
     uint256 amount = users[_msgSender()].refAmounts;
-    require(amount > 0, "ETH");
+    require(amount > 0, "Error::SmartLand, Zero Interest!");
 
     users[_msgSender()].refAmounts = 0;
     _safeTransferETH(_msgSender(), amount);
   }
 
-  function exists(address account) public view returns (bool) {
+  function exist(address account) public view returns (bool) {
     return users[account].referrer != address(0);
+  }
+
+  function notExist(address account) public view returns (bool) {
+    return users[account].referrer == address(0);
   }
 
   function _beforeTokenTransfer(
     address from,
     address to,
     uint256 tokenId
-  ) internal override whenNotPaused {
+  ) internal override {
+    if (from == address(0)) {
+      require(totalSupply() < maxSupply, "Error::SmartLand, Max Supply!");
+    } else {
+      require(!PAUSED, "Error::SmartLand, Transfer Paused!");
+    }
+
     super._beforeTokenTransfer(from, to, tokenId);
   }
 
--- contracts/SmartLand/SmartSecure.sol
+++ contracts/SmartLand/SmartSecure.sol
@@ -1,18 +1,35 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.7;
 
-import "@openzeppelin/contracts/access/Ownable.sol";
-import "@openzeppelin/contracts/security/Pausable.sol";
+import "@openzeppelin/contracts/utils/Context.sol";
 
-abstract contract SmartSecure is Ownable, Pausable {
+abstract contract SmartSecure is Context {
+  event Paused(address account);
+  event Unpaused(address account);
   event AddedBlackList(address indexed user);
   event RemovedBlackList(address indexed user);
 
-  bool internal locked;
+  modifier onlyOwner() {
+    require(owner == _msgSender(), "Error::SmartSecure, Only from Owner");
+    _;
+  }
+
+  modifier onlyGameMaster() {
+    require(gameMaster == _msgSender(), "Error::SmartSecure, Only from Game Master");
+    _;
+  }
+
+  bool internal LOCKED;
+
+  bool internal PAUSED;
 
   string internal BASE_URL =
     "ipfs://bafybeigqzp3iulfmhrnrrhssfbehzompxkv5g4xgsghcjzn3jic5bkukca/";
   string internal BASE_EXT = ".json";
+
+  address public owner;
+
+  address public gameMaster;
 
   uint256 public LAND_PRICE = 1 ether;
 
@@ -20,33 +37,45 @@ abstract contract SmartSecure is Ownable, Pausable {
 
   mapping(address => bool) public blacklist;
 
+  mapping(uint256 => bytes) public landData;
+
   modifier nonReentrant() {
-    require(!blacklist[_msgSender()], "BLK");
-    require(!locked, "LCK");
-    locked = true;
+    require(!blacklist[_msgSender()], "Error::SmartSecure, User blacklisted!");
+    require(!LOCKED, "Error::SmartSecure, Locked!");
+    LOCKED = true;
     _;
-    locked = false;
+    LOCKED = false;
   }
 
   function transferToOwner() internal {
-    _safeTransferETH(owner(), LAND_PRICE);
+    _safeTransferETH(owner, LAND_PRICE);
   }
 
   function _safeTransferETH(address to, uint256 value) internal {
     (bool success, ) = to.call{gas: 23000, value: value}("");
 
-    require(success, "ETH");
-  }
-
-  function pause() public onlyOwner {
-    _pause();
-  }
-
-  function unpause() public onlyOwner {
-    _unpause();
+    require(success, "Error::SmartSecure, Transfer Failed!");
   }
 
   //only owner
+  function lock() public onlyOwner {
+    LOCKED = true;
+  }
+
+  function pause() public onlyOwner {
+    PAUSED = true;
+    emit Paused(_msgSender());
+  }
+
+  function unLock() public onlyOwner {
+    LOCKED = false;
+  }
+
+  function unpause() public onlyOwner {
+    PAUSED = false;
+    emit Unpaused(_msgSender());
+  }
+
   function setPrice(uint256 newPrice) public onlyOwner {
     LAND_PRICE = newPrice;
   }
@@ -63,6 +92,31 @@ abstract contract SmartSecure is Ownable, Pausable {
     REFERRAL_PERCENT = percent;
   }
 
+  function setOwner(address newOwner) public onlyOwner {
+    require(newOwner != address(0), "Error::SmartSecure, Zero Address!");
+    owner = newOwner;
+  }
+
+  function setGameMaster(address newOwner) public onlyOwner {
+    require(newOwner != address(0), "Error::SmartSecure, Zero Address!");
+    gameMaster = newOwner;
+  }
+
+  function updateLandData(uint256 tokenId, bytes memory data) public onlyGameMaster {
+    landData[tokenId] = data;
+  }
+
+  function updateBatchLandData(uint256[] memory tokenIds, bytes[] memory data)
+    public
+    onlyGameMaster
+  {
+    require(tokenIds.length == data.length, "Error::SmartSecure, Incorrect Data!");
+    for (uint256 i = 0; i < tokenIds.length; i++) {
+      uint256 tokenId = tokenIds[i];
+      landData[tokenId] = data[i];
+    }
+  }
+
   function addBlackList(address user) external onlyOwner {
     blacklist[user] = true;
     emit AddedBlackList(user);
@@ -74,6 +128,6 @@ abstract contract SmartSecure is Ownable, Pausable {
   }
 
   function withdrawBnb(uint256 value) external onlyOwner {
-    payable(owner()).transfer(value);
+    payable(owner).transfer(value);
   }
 }
